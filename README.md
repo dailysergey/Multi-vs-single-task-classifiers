48fc3bea2d4f784c161295961e1742cf93bd44e2
s.guskov <s.guskov@innopolis.ru>
2023-05-31 01:50:35 +0300

    single task checks

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Start
+# Multi-vs-single-task-classifiers
+
+Install `pip install -r requirements.txt -q`
+
+# Single task classifier 
+Запуска процесса с обучением начинается с config файла
+- выбрать модель
+- выбрать одну из предложенных задач "sst2", "mrpc", "cola"
+- настроить device
+- логировать в w&b
+- 
+
+# Multi task classifier
+
+
---
+++ single-task-classifier/transformer_glue.py
@@ -0,0 +1,152 @@
+import os
+import numpy as np
+import pandas as pd
+import wandb
+import logging
+import torch
+import hydra
+from omegaconf import DictConfig, OmegaConf
+from hydra.utils import instantiate
+from datetime import datetime
+from argparse import ArgumentParser
+from datasets import load_dataset, load_metric
+from transformers import (AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding,
+                          Trainer, TrainingArguments, EarlyStoppingCallback)
+
+
+ACCURACY = PRECISION = RECALL = F1 = None
+
+
+def get_args():
+    '''Load args from config.yaml'''
+    config = instantiate(OmegaConf.load("config.yaml"))
+    return config
+
+
+def save_config(cfg: DictConfig, output_dir: str):
+    config_file = os.path.join(output_dir, "config.yaml")
+    os.makedirs(output_dir, exist_ok=True)
+    with open(config_file, "w") as f:
+        f.write(OmegaConf.to_yaml(cfg))
+
+
+def compute_metrics(eval_pred, average="binary"):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    metric_dict = {}
+    for metric in [ACCURACY, PRECISION, RECALL, F1]:
+        metric_dict.update(metric.compute(
+            predictions=predictions, references=labels))
+    return metric_dict
+
+
+def log_metrics(metrics, filename, task, model, seed):
+    '''
+    Log metrics into pandas DataFrame
+    '''
+    metrics_df = pd.DataFrame({
+        "model": [model],
+        "task": [task],
+        "seed": [seed],
+        "train_accuracy": [metrics["train_accuracy"]],
+        "test_accuracy": [metrics["test_accuracy"]],
+        "train_f1": [metrics["train_f1"]],
+        "test_f1": [metrics["test_f1"]]
+    })
+
+    if os.path.isfile(filename):
+        metrics_df = pd.concat([pd.read_csv(filename), metrics_df])
+
+    metrics_df.to_csv(filename, index=False)
+
+
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def main(cfg: DictConfig):
+    now = datetime.now()
+    date = now.strftime('%Y-%m-%d')
+    time = now.strftime("%H-%M-%S")
+    hydra_cfg = hydra.core.hydra_config.HydraConfig.get()
+
+    args = get_args()
+    logging.basicConfig(format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
+    
+    logging.info(
+        f"Current hydra folder:{hydra_cfg['runtime']['output_dir']}")
+
+    device = torch.device(
+        args.device) if torch.cuda.is_available() else torch.device('cpu')
+    logging.info(f"Device: {device}")
+    # Load model and tokenizer from Huggingface Hub
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.model, padding=True, truncation=True)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        args.model).to(device)
+
+    # Load and preprocess dataset
+    dataset = load_dataset("glue", args.task)
+    if args.task == "mrpc":
+        dataset = dataset.map(lambda x: {"sentence": x["sentence1"] + " <s> " + x["sentence2"]},
+                              batched=False, load_from_cache_file=False)
+
+    tokenized = dataset.map(lambda examples: tokenizer(examples["sentence"]), batched=True,
+                            load_from_cache_file=False).remove_columns(["idx", "sentence"])
+
+    # Load metrics
+    global ACCURACY, PRECISION, RECALL, F1
+    ACCURACY = load_metric("accuracy")
+    PRECISION = load_metric("precision")
+    RECALL = load_metric("recall")
+    F1 = load_metric("f1")
+
+    # Fix training settings
+    settings = args.TRAINING_ARGS.copy()
+    settings["num_train_epochs"] = args.epochs
+    settings["seed"] = args.seed
+    settings['output_dir'] = hydra_cfg['runtime']['output_dir']
+
+    # Create wandb run if needed
+    if args.use_wandb:
+        run = wandb.init(
+            project=args.wandb_project,
+            entity=args.wandb_entity,
+            tags=[args.model, args.task],
+            name=f"{args.model}_{args.task}"
+        )
+
+    callbacks = [EarlyStoppingCallback(early_stopping_patience=3)]
+
+    # Start training
+    collator = DataCollatorWithPadding(
+        tokenizer=tokenizer, padding="longest")
+    trainer = Trainer(
+        model=model,
+        args=TrainingArguments(**settings),
+        train_dataset=tokenized["train"],
+        eval_dataset=tokenized["validation"],
+        data_collator=collator,
+        compute_metrics=compute_metrics,
+        callbacks=callbacks
+    )
+    trainer.train()
+
+    # Collect and log metrics
+    final_metrics = trainer.evaluate(
+        tokenized["train"], metric_key_prefix="train")
+    final_metrics.update(trainer.evaluate(
+        tokenized["validation"], metric_key_prefix="test"))
+    log_metrics(final_metrics, args.log_file,
+                args.task, args.model, args.seed)
+    if args.use_wandb:
+        wandb.log(final_metrics)
+        run.finish()
+    else:
+        logging.info("Final metrics:")
+        logging.info(final_metrics)
+    save_config(OmegaConf.to_container(
+        args, resolve=True), args.TRAINING_ARGS.output_dir)
+
+
+if __name__ == "__main__":
+    main()
